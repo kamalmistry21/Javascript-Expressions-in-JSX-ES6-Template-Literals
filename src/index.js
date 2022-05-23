bbb67ff3ac4d44aeef63269b5c03d61b71207782
Kamal Mistry <31496227+kamalmistry21@users.noreply.github.com>
2022-05-23 22:08:49 +1200

    Adding Project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+//If you want to add a variable/js expressions you have to put {} around the variable/js expressions
+const fName = "Kamal";
+const lName = "Mistry";
+const luckyNumber = 9;
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {`${fName} ${lName}`}</h1>
+    <p>Your lucky number is {luckyNumber}</p>
+  </div>,
+  document.getElementById("root")
+);
